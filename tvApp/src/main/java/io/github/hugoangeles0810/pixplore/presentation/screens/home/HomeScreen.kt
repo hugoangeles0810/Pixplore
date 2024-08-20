1dd9a4fca595648d90e07dad521bcfc80284d47d
Hugo Angeles <hugoangeles0810@gmail.com>
2024-08-20 16:49:40 -0500

    Home screen uses PictureCard

--- tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/home/HomeScreen.kt
+++ tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/home/HomeScreen.kt
@@ -2,10 +2,12 @@ package io.github.hugoangeles0810.pixplore.presentation.screens.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.tv.material3.Text
+import io.github.hugoangeles0810.pixplore.presentation.components.PictureCard
 
 @Composable
 fun HomeScreen() {
@@ -13,6 +15,13 @@ fun HomeScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Home")
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3)
+        ) {
+            items(100) {
+                PictureCard(title = "Picture $it", subtitle = "01/01/10", imageUrl = "")
+            }
+        }
     }
 }
