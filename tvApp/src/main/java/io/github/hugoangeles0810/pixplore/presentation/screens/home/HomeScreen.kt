e2f97d0e2d99a75d21e7a410e7f5354aaf77347d
Hugo Angeles <hugoangeles0810@gmail.com>
2024-08-22 13:24:55 -0500

    Refactoring: creating PhotoGrid component

--- tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/home/HomeScreen.kt
+++ tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/home/HomeScreen.kt
@@ -1,29 +1,19 @@
 package io.github.hugoangeles0810.pixplore.presentation.screens.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import io.github.hugoangeles0810.pixplore.presentation.components.Error
 import io.github.hugoangeles0810.pixplore.presentation.components.Loading
-import io.github.hugoangeles0810.pixplore.presentation.components.NextPhotosPageSkeleton
-import io.github.hugoangeles0810.pixplore.presentation.components.PhotoCard
+import io.github.hugoangeles0810.pixplore.presentation.components.PhotoGrid
 
-
-private const val Columns = 3
-private val GridSeparation = 20.dp
 
 @Composable
 fun HomeScreen(
@@ -37,29 +27,7 @@ fun HomeScreen(
     ) {
         when (val s = uiState) {
             is HomeScreenUiState.Ready -> {
-
-                val lazyPagingItems = s.photos.collectAsLazyPagingItems()
-
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(Columns),
-                    verticalArrangement = Arrangement.spacedBy(GridSeparation),
-                    horizontalArrangement = Arrangement.spacedBy(GridSeparation),
-                    contentPadding = PaddingValues(top = 32.dp)
-                ) {
-                    items(lazyPagingItems.itemCount) { index ->
-                        lazyPagingItems[index]?.let {
-                            PhotoCard(user = it.username, createdAt = it.createdAt, url = it.url)
-                        }
-                    }
-
-                    lazyPagingItems.apply {
-                        if (loadState.append is LoadState.Loading) {
-                            item {
-                                NextPhotosPageSkeleton(columns = Columns)
-                            }
-                        }
-                    }
-                }
+                PhotoGrid(lazyPagingItems = s.photos.collectAsLazyPagingItems())
             }
             is HomeScreenUiState.Loading -> {
                 Loading(modifier = Modifier.fillMaxSize())
