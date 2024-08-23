f0ea6fe75f560e9346aeca8ee228becd483657d3
Hugo Angeles <hugoangeles0810@gmail.com>
2024-08-23 01:08:14 -0500

    Default home back navigation

---
+++ tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/components/BackPressHandledArea.kt
@@ -0,0 +1,31 @@
+package io.github.hugoangeles0810.pixplore.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+
+@Composable
+fun BackPressHandledArea(
+    onBackPressed: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit,
+) =
+    Box(
+        modifier = Modifier
+            .onPreviewKeyEvent {
+                if (it.key == Key.Back && it.type == KeyEventType.KeyUp) {
+                    onBackPressed()
+                    true
+                } else {
+                    false
+                }
+            }
+            .then(modifier),
+        content = content
+    )
