0da5b9a1b77e98f9f7f2235a3ec1bc4125564866
Hugo Angeles <hugoangeles0810@gmail.com>
2024-08-23 00:20:30 -0500

    Adding about screen

--- tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/about/AboutScreen.kt
+++ tvApp/src/main/java/io/github/hugoangeles0810/pixplore/presentation/screens/about/AboutScreen.kt
@@ -1,18 +1,39 @@
 package io.github.hugoangeles0810.pixplore.presentation.screens.about
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
+import io.github.hugoangeles0810.pixplore.R
+import io.github.hugoangeles0810.pixplore.presentation.components.PixploreLogo
 
 @Composable
 fun AboutScreen() {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "About")
+        PixploreLogo(modifier = Modifier.width(130.dp))
+        Spacer(modifier = Modifier.height(58.dp))
+        Text(
+            text = stringResource(id =  R.string.about_screen_description),
+            style = MaterialTheme.typography.headlineSmall
+
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.about_screen_made_by),
+            style = MaterialTheme.typography.labelMedium
+        )
     }
 }
